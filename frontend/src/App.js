d9ca30e0dbf8575dbb85c288a4137f59ef1d5576
Mohamed Mussa <mussaop812@gmail.com>
2026-04-14 19:49:06 -0400

    set timer for clock in and clock out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,26 @@ import { useState } from 'react';
 
 function App() {
   const [studentId, setStudentId] = useState('');
+  const [message, setMessage] = useState('');
+
+  function handleClockIn(){
+  setMessage('✅ Clocked in: ' + studentId )
+  setTimeout(() => {
+    setStudentId('');
+    setMessage('');
+
+  }, 3000)
+}
+
+function handleClockOut(){
+  setMessage('🔴 Clocked out: ' + studentId);
+  setTimeout(()=>{
+    setStudentId('');
+    setMessage('');
+  } , 3000)
+}
+
+
 
   return (
     <div>
@@ -14,10 +34,13 @@ function App() {
         onChange={(e) => setStudentId(e.target.value)}
       />
 
-      <button disabled={studentId === ''}>Clock In</button>
-      <button disabled={studentId === ''}>Clock Out</button>
+      <button disabled={studentId === ''} onClick={handleClockIn}>Clock In</button>
+      <button disabled={studentId === ''} onClick={handleClockOut}>Clock Out</button>
+      <p>{message}</p>
     </div>
   );
 }
+
+
 
 export default App;
