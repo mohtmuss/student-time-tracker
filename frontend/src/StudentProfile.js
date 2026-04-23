00278d49ad0721347dd5b4522908e3228b13ae7a
Mohamed Mussa <mussaop812@gmail.com>
2026-04-22 23:03:24 -0400

    v.4

--- frontend/src/StudentProfile.js
+++ frontend/src/StudentProfile.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback} from 'react';
 
 function StudentProfile({ student, onBack, refreshClockedIn }) {
   const [history, setHistory] = useState([]);
